4728fe371a409e7618f87df400ad800cff179631
Abhirup Das <147976109+abhirup0421L@users.noreply.github.com>
2026-05-10 21:18:17 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -10,16 +10,26 @@ client = MongoClient(MONGO_URI)
 
 db = client["resume_ai"]
 
+# COLLECTIONS
 users_collection = db["users"]
+
 cache_collection = db["resume_cache"]
+
 api_usage_collection = db["api_usage"]
+
 coupons_collection = db["coupons"]
 
+interview_progress_collection = db["interview_progress"]
+
+settings_collection = db["settings"]
+
+# AUTO DELETE CACHE AFTER 10 MINUTES
 cache_collection.create_index(
     "created_at",
     expireAfterSeconds=600
 )
 
+# AUTO DELETE EXPIRED COUPONS
 coupons_collection.create_index(
     "expires_at",
     expireAfterSeconds=0
