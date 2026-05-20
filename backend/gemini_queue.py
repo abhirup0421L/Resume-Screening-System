67c00c889b215c13be5b2ccef1e51d5c8423ea1c
Abhirup Das <147976109+abhirup0421L@users.noreply.github.com>
2026-05-20 20:48:46 +0530

    Update gemini_queue.py

--- backend/gemini_queue.py
+++ backend/gemini_queue.py
@@ -1,13 +1,14 @@
-# backend/gemini_queue.py
-
 import time
 from datetime import datetime, timedelta
 from backend.database import api_usage_collection
 
 MAX_REQUESTS_PER_MINUTE = 15
+MAX_WAIT_SECONDS = 20
 
 
 def wait_for_gemini_slot():
+    start_time = time.time()
+
     while True:
         now = datetime.utcnow()
         one_minute_ago = now - timedelta(seconds=60)
@@ -26,4 +27,7 @@ def wait_for_gemini_slot():
             })
             return True
 
-        time.sleep(4)
+        if time.time() - start_time > MAX_WAIT_SECONDS:
+            return False
+
+        time.sleep(3)
